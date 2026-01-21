e4a933bde4d654162b6a538531c9c8fc77d4e29f
Marcela Sutić <marcela@marcelasutic.com>
2026-01-21 11:13:17 +0100

    Initial commit

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,38 @@
+class MoviesController < ApplicationController
+    def index
+          @movies = Movie.all
+    end
+
+    def show 
+          @movie = Movie.find(params[:id])
+    end
+
+    def edit 
+          @movie = Movie.find(params[:id])
+    end
+
+    def update
+        @movie = Movie.find(params[:id])
+        movie_params =params.require(:movie).permit(:title, :description, :rating, :released_on, :total_gross)
+       
+        @movie.update(movie_params)
+        redirect_to movie_path(@movie)
+    end
+
+    def new
+        @movie = Movie.new
+    end 
+
+    def create 
+      movie_params =params.require(:movie).permit(:title, :description, :rating, :released_on, :total_gross)
+      @movie = Movie.new(movie_params)      
+      @movie.save
+      redirect_to movies_path
+    end
+
+    def destroy 
+        @movie = Movie.find(params[:id])
+        @movie.destroy
+        redirect_to movies_url, status: :see_other
+        end
+end
