68405b298f50a514c96c8dc24a308e4825219978
Marcela Sutić <marcela@marcelasutic.com>
2026-01-26 13:51:53 +0100

    committing error messages

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -14,9 +14,11 @@ class MoviesController < ApplicationController
     def update
         @movie = Movie.find(params[:id])
         movie_params =params.require(:movie).permit(:title, :description, :rating, :released_on, :total_gross, :director, :duration, :image_file_name)
-       
-        @movie.update(movie_params)
-        redirect_to movie_path(@movie)
+        if @movie.update(movie_params)
+           redirect_to movie_path(@movie)
+        else 
+           render :edit, status: :unprocessable_entity
+        end
     end
 
     def new
@@ -26,8 +28,11 @@ class MoviesController < ApplicationController
     def create 
       movie_params =params.require(:movie).permit(:title, :description, :rating, :released_on, :total_gross, :director, :duration, :image_file_name)
       @movie = Movie.new(movie_params)      
-      @movie.save
-      redirect_to movies_path
+        if @movie.save 
+           redirect_to movies_path
+        else 
+           render :new, status: :unprocessable_entity
+      end
     end
 
     def destroy 
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,11 +1,20 @@
 class Movie < ApplicationRecord
 
+  RATINGS = %w(G PG PG-13 R NC-17)
+
   validates :title, :released_on, :duration, presence: true
 
   validates :description, length: { minimum: 25 }
 
-  validates :total_gross, numricality : { greater_than : 0}
-  
+  validates :total_gross, numericality: { greater_than: 0 }
+
+  validates :image_file_name, format: {
+    with: /\w+\.(jpg|png)\z/i,
+    message: "must be a JPG or PNG image file"
+  }
+
+  validates :rating, inclusion: { in: RATINGS }
+
   def flop?
     total_gross.blank? || total_gross < 225_000_000
   end
