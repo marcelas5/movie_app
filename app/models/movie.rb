275a0a6e3fc35742e0c62150b418ef4f91d2fdc0
Marcela Sutić <marcela@marcelasutic.com>
2026-01-26 11:09:35 +0100

    committing model validations

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,11 @@
 class Movie < ApplicationRecord
+
+  validates :title, :released_on, :duration, presence: true
+
+  validates :description, length: { minimum: 25 }
+
+  validates :total_gross, numricality : { greater_than : 0}
+  
   def flop?
     total_gross.blank? || total_gross < 225_000_000
   end
