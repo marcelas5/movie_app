13e8a108ad81d15a6243893086b48695d4fd13fb
Marcela Sutić <marcela@marcelasutic.com>
2026-01-28 11:42:22 +0100

    flash messages and a reviews feature

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -15,7 +15,7 @@ class MoviesController < ApplicationController
         @movie = Movie.find(params[:id])
         movie_params =params.require(:movie).permit(:title, :description, :rating, :released_on, :total_gross, :director, :duration, :image_file_name)
         if @movie.update(movie_params)
-           redirect_to movie_path(@movie)
+           redirect_to movie_path(@movie), notice: "Movie updated successfully"
         else 
            render :edit, status: :unprocessable_entity
         end
@@ -29,7 +29,7 @@ class MoviesController < ApplicationController
       movie_params =params.require(:movie).permit(:title, :description, :rating, :released_on, :total_gross, :director, :duration, :image_file_name)
       @movie = Movie.new(movie_params)      
         if @movie.save 
-           redirect_to movies_path
+           redirect_to movies_path, notice: "Movie created successfully"    
         else 
            render :new, status: :unprocessable_entity
       end
@@ -38,6 +38,6 @@ class MoviesController < ApplicationController
     def destroy 
         @movie = Movie.find(params[:id])
         @movie.destroy
-        redirect_to movies_url, status: :see_other
+        redirect_to movies_url, status: :see_other, alert: "Movie deleted successfully"
         end
 end
