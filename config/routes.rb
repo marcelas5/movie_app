4bc3db2cc9ed6680e5e6f478a736b20e15b7ce3b
Marcela Sutić <marcela@marcelasutic.com>
2026-02-05 11:18:58 +0100

    committing session

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  
   resources :movies do 
     resources :reviews
   end
 
+  resource :session, only: [:new, :create, :destroy]
+  
   resources :users
   get "signup" => "users#new"
     
