a2a463bf1f049126b971f120cc0ba791d090a54b
Marcela Sutić <marcela@marcelasutic.com>
2026-02-05 14:40:00 +0100

    committing changes to session controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,18 @@
 class SessionsController < ApplicationController
 
   def new
-
+    
   end
 
   def create
-
+    user = User.find_by(email: params[:email])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to user, notice: "Welcome back, #{user.name}!"
+    else
+      flash.now[:alert] = "Email or password is invalid"
+      render :new, status: :unprocessable_entity    
+    end
   end
 
   def destroy
