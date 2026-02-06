56e4397da8bbf49275cada85cc6c09d7a825f1f5
Marcela Sutić <marcela@marcelasutic.com>
2026-02-06 14:27:40 +0100

    add permission

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,19 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+private
+
+  def require_signin
+    unless current_user
+      redirect_to new_session_url, alert: "You must be signed in to access this page."
+    end
+  end
+
+  def current_user
+    @current_user ||=User.find(session[:user_id]) if session[:user_id]
+  end
+
+  helper_method :current_user
+
 end
