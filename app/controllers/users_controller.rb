403c25a7313f85878d178347d8e12641585d7474
Marcela Sutić <marcela@marcelasutic.com>
2026-02-06 12:13:45 +0100

    add sign in changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,6 +15,7 @@ end
 def create
   @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       redirect_to @user, notice: "User successfully created."
     else
       render :new, status: :unprocessable_entity
