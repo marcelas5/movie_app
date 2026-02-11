a743bf76613969fdf051fb671ee56961ce8ac312
Marcela Sutić <marcela@marcelasutic.com>
2026-02-11 14:49:44 +0100

    Add fans

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -3,8 +3,8 @@ class Movie < ApplicationRecord
   RATINGS = %w(G PG PG-13 R NC-17)
 
   has_many :reviews, dependent: :destroy
-
   has_many :favorites, dependent: :destroy
+  has_many :fans, through: :favorites, source: :user
 
   validates :title, :released_on, :duration, presence: true
 
