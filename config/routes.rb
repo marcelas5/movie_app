432e270f1f85a0f3be1ff2ef4d0167d8be827877
Marcela Sutić <marcela@marcelasutic.com>
2026-02-13 14:04:40 +0100

    Add favorites

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  resources :favorites
   
   resources :movies do 
     resources :reviews
+    resources :favorites, only: [:create, :destroy]
   end
 
   resource :session, only: [:new, :create, :destroy]
